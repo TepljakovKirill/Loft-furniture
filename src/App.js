1d1c36b75b0d3398ff4d034b2903a18f505b0b84
Kirill Tepljakov <super.css2017@yandex.ru>
2022-12-25 14:34:43 +0300

    выполнен чёрный header

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./components/Header/header";
+import "./scss/app.scss";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <header>
+        <Header />
+      </header>
+    </div>
+  );
+}
+
+export default App;
